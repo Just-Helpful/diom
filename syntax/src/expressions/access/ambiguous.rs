ed994d9a3d329244bffaef4a42262277d74b3dff
Alex Colby <alexanderdcolby@gmail.com>
2024-06-22 19:05:32 +0100

    docs: small tweaks to `Ambiguous` syntax node

--- syntax/src/expressions/access/ambiguous.rs
+++ syntax/src/expressions/access/ambiguous.rs
@@ -3,17 +3,19 @@ use crate::{expressions::Expression, ident::Ident};
 /// A node to represent a syntax that could be either a
 /// method call or field access.
 ///
-/// More specifically, the syntax that causes issues is `{Expression} {Ident}`,<br>
+/// More specifically, the ambiguous syntax is `{Expression} {Ident}`,<br>
 /// as it could be either intepreted as:
+///
 /// - `{Expression}.{Ident}`, i.e. a field access
 /// - `{Expression}({Ident})`, i.e. a function call with a variable
 ///
 /// For example `let y = 3; x y` can be interpreted as either:
-/// 1. `x.y` if `x: {y: Float}`
-/// 2. `x(y)` if `x: (y: Float): ()`
 ///
-/// This ambiguity **should** be resolved by typechecking, i.e. this node
-/// shouldn't exist in the syntax tree after the typechecking stage.
+/// - `x.y` if `x: {y: Float}`
+/// - `x(y)` if `x: (y: Float): ()`
+///
+/// This ambiguity **should** be resolved by typechecking, i.e. this node<br>
+/// **shouldn't** exist in the syntax tree after the typechecking stage.
 pub struct Ambiguous<I> {
   pub value: Box<Expression<I>>,
   pub name: Ident<I>,
