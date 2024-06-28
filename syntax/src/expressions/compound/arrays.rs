4428cdfa3c66e8ca32918d2d7599ae84a7fdf46d
Alex Colby <alexanderdcolby@gmail.com>
2024-06-28 22:12:46 +0100

    tweak: adds named array expressions

--- syntax/src/expressions/compound/arrays.rs
+++ syntax/src/expressions/compound/arrays.rs
@@ -1,9 +1,10 @@
-use crate::InfoSource;
+use crate::{ident::Ident, InfoSource};
 
 use super::Expression;
 
 #[derive(InfoSource)]
 pub struct Array<I> {
+  pub name: Option<Ident<I>>,
   pub contents: Vec<Expression<I>>,
   pub info: I,
 }
