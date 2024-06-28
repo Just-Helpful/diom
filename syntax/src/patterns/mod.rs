72bf5530b5678846ef38c0ca8852f001c7ce08b8
Alex Colby <alexanderdcolby@gmail.com>
2024-06-28 19:13:29 +0100

    tweak: removes `Enum` pattern - this pattern is identical to those used for `Struct`s, `Array`s and `Tuple`s, as `Enum` patterns just consist of a path, then a variant - the enum type has to be resolved by the type checker

--- syntax/src/patterns/mod.rs
+++ syntax/src/patterns/mod.rs
@@ -1,7 +1,5 @@
 pub mod arrays;
 use arrays::Array;
-pub mod enums;
-use enums::Enum;
 pub mod rest;
 use rest::Rest;
 pub mod structs;
@@ -14,7 +12,6 @@ pub mod tuples;
 #[derive(InfoSource)]
 pub enum Pattern<I> {
   Array(Array<I>),
-  Enum(Enum<I>),
   Struct(Struct<I>),
   Tuple(Tuple<I>),
   Var(Ident<I>),
