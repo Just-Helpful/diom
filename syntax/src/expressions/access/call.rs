17af9cbfa5f33b5e5a067ab91bfb8d8579132ed7
Alex Colby <alexanderdcolby@gmail.com>
2024-09-29 09:34:41 +0100

    tweak: renames `Call` node fields for better compatibility with other nodes

--- syntax/src/expressions/access/call.rs
+++ syntax/src/expressions/access/call.rs
@@ -3,7 +3,7 @@ use diom_info::{InfoMap, InfoRef, InfoSource};
 
 #[derive(Clone, InfoSource, InfoRef, InfoMap)]
 pub struct Call<I> {
-  pub function: Box<Expression<I>>,
-  pub arguments: Vec<Expression<I>>,
+  pub value: Box<Expression<I>>,
+  pub args: Vec<Expression<I>>,
   pub info: I,
 }
