05ee58a76d767c2b100698cf9871b8dd3ae4747d
Alex Colby <alexanderdcolby@gmail.com>
2024-09-29 09:31:46 +0100

    feat: adds information fetching to `Path`s

--- syntax/src/path.rs
+++ syntax/src/path.rs
@@ -1,7 +1,7 @@
 use crate::ident::Ident;
-use diom_info::{InfoMap, InfoSource};
+use diom_info::{InfoMap, InfoRef, InfoSource};
 
-#[derive(Clone, InfoSource, InfoMap)]
+#[derive(Clone, InfoSource, InfoRef, InfoMap)]
 pub struct Path<I> {
   pub segments: Vec<Ident<I>>,
   pub info: I,
