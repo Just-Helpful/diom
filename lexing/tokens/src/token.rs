7f6986780cb11b10b6ed4500a1013c1a2bd12091
Alex Colby <alexdcolby@pm.me>
2025-11-20 10:36:42 +0000

    docs: adds documentation for token variants

--- lexing/tokens/src/token.rs
+++ lexing/tokens/src/token.rs
@@ -10,45 +10,81 @@
 /// final. If you do, make sure that tests **still pass**.
 #[derive(Debug, Clone, PartialEq)]
 pub enum Token {
-  // Brackets
+  /* Brackets */
+  /// A left parenthesis `(`
   LParen,
+  /// A right parenthesis `)`
   RParen,
+  /// A left curly bracket `{`
   LCurly,
+  /// A right curly bracket `}`
   RCurly,
+  /// A left square bracket `[`
   LBrace,
+  /// A right square bracket `]`
   RBrace,
-  // Punctuation
+
+  /* Punctuation */
+  /// A single dot `.` (for field / method access)
   Dot,
+  /// A semicolon `;` (for line termination)
   Semi,
+  /// A colon `:` (for struct fields)
   Colon,
+  /// A comma `,` (for tuples / arrays)
   Comma,
+  /// An equals sign `=` (for variable assignment)
   Assign,
+  /// Ellipses `...` (for "rest" parameters)
   Ellipses,
+  /// Function arrow `=>` (for anonymous functions)
   Function,
-  // Reserved keywords
+
+  /* Reserved keywords */
+  /// A `let` keyword (for variable definition)
   Let,
+  /// A `return` keyword (for early returns)
   Return,
-  // Operators
+
+  /* Operators */
+  /// A not `!` operator
   Not,
+  /// An and `&` operator
   And,
+  /// An or `|` operator
   Or,
+  /// A plus `+` operator
   Plus,
+  /// A minus `-` operator
   Minus,
+  /// A times `*` operator
   Times,
+  /// A divide `/` operator
   Divide,
+  /// An equality `==` operator
   Eq,
+  /// An inverse equality `!=` operator
   Ne,
+  /// A less than `<` operator
   Lt,
+  /// A greater than `>` operator
   Gt,
+  /// A less than / equal to `<=` operator
   LtEq,
+  /// A greater than / equal to `>=` operator
   GtEq,
+  /// A monadic application `?` operator
   Monad,
-  // String-like
-  StringIdent(Box<str>),
-  Char(char),
-  Comment(Box<str>),
-  // Value-like
+  // Literals
+  /// A floating point value, i.e. `2.34`, `-0.02`
   Float(f64),
+  /// A single character, i.e. `'x'`, `'\u+26c4'`
+  Char(char),
+  // String-like
+  /// An identifier, i.e. `x`, `_`, `string_ident_0`
+  StringIdent(Box<str>),
+  /// A single / multiline comment
+  Comment(Box<str>),
 }
 
 impl Token {
