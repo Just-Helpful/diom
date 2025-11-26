a9c89d0bfcab6f93230a68c4e51ec2c2b03eea38
Alex Colby <alexdcolby@pm.me>
2025-11-26 18:53:57 +0000

    feat: adds command line interface for running interpreted code

---
+++ backends/interpreter-cli/src/main.rs
@@ -0,0 +1,94 @@
+//! Diom code evaluation via the `interpreter` backend.
+use std::fs::read_to_string;
+use std::io;
+use std::path::Path;
+use std::str::FromStr;
+
+use clap::{Args, Parser};
+use diom_interpreter::interpret_expr;
+use diom_lexer::parse_tokens;
+use diom_parser::expressions::parse_expression;
+use diom_tokens::SpanTokens;
+
+/// Interprets and executes the Diom language
+#[derive(Debug, Parser)]
+#[command(version)]
+#[command(arg_required_else_help = true)]
+struct MainArgs {
+  /// Program code to evaluate
+  #[command(flatten)]
+  source: SourceArgs,
+}
+
+#[derive(Debug, Args)]
+#[group(required = true, multiple = false)]
+struct SourceArgs {
+  /// A path source starting with `/`, `./` or raw code
+  source: Option<ProgramSource>,
+
+  /// The code to evaluate
+  #[arg(short, long)]
+  eval: Option<String>,
+
+  /// The path to a file to evaluate
+  #[arg(short, long)]
+  file: Option<String>,
+}
+
+/// The source to run the program from
+#[derive(Debug, Clone)]
+pub enum ProgramSource {
+  /// Evaluate a string from the terminal
+  Eval(String),
+  /// Load a program from a file and evaluate it
+  File(String),
+}
+
+impl FromStr for ProgramSource {
+  type Err = &'static str;
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    use ProgramSource::*;
+    let is_path = s.starts_with("/") || s.starts_with("./");
+    let variant = if is_path { File } else { Eval };
+    Ok(variant(s.to_owned()))
+  }
+}
+
+impl From<SourceArgs> for ProgramSource {
+  fn from(SourceArgs { source, eval, file }: SourceArgs) -> Self {
+    let src_e = eval.map(ProgramSource::Eval);
+    let src_f = file.map(ProgramSource::File);
+    source
+      .xor(src_e)
+      .xor(src_f)
+      .expect("Only one source to be specified")
+  }
+}
+
+impl ProgramSource {
+  pub fn fetch(self) -> io::Result<String> {
+    use ProgramSource::*;
+    match self {
+      Eval(code) => Ok(code),
+      File(path) => read_to_string(Path::new(&path)),
+    }
+  }
+}
+
+fn main() {
+  let args = MainArgs::parse();
+
+  let src = ProgramSource::from(args.source);
+  let code = src.fetch().unwrap();
+
+  let (input, tokens) = parse_tokens(code.as_str().into()).unwrap();
+  assert!(input.is_empty());
+  println!("tokens = {tokens:?}");
+
+  let (input, expr) = parse_expression(SpanTokens::from(&tokens)).unwrap();
+  assert!(input.is_empty());
+  println!("expr = {expr:?}");
+
+  let value = interpret_expr(expr).unwrap();
+  println!("{value:?}");
+}
