085b60a01de6536f5130f3449a93a2ee935204ba
Alex Colby <alexdcolby@pm.me>
2026-03-07 16:48:12 +0000

    feat: adds config options for displaying spans
    it's now possible to customise the characters for bracketing

--- fmt/src/formats/spans.rs
+++ fmt/src/formats/spans.rs
@@ -2,21 +2,111 @@ use super::LineWriter;
 use crate::{formats::lines::Lines, Flush, Format, Write};
 use std::{cell::RefCell, ops::Range, rc::Rc};
 
-#[derive(Default, Clone, Copy)]
-pub struct Spans;
+/// Displays the code spans associated with a particular code fragment.\
+/// The code spans represent the syntax nodes that cover the fragment.
+///
+/// ## Example
+///
+/// ```ignore
+/// let expr = Infix {value: Var("counter"), op: Add, other: Float(1.0)};
+/// expr.display::<Spans>()
+/// // "(counter + 1.0)"
+/// //   ( var )   ( )
+/// //   (    add    )
+/// //  (    group    )
+/// ```
+#[derive(Clone, Copy)]
+pub struct Spans {
+  pub single: char,
+  pub open: char,
+  pub close: char,
+  pub fill: char,
+}
+
+impl Default for Spans {
+  fn default() -> Self {
+    Self {
+      single: '^',
+      open: '(',
+      close: ')',
+      fill: ' ',
+    }
+  }
+}
 
 impl Format for Spans {
   type Writer<W: Write> = SpanWriter<W>;
 
   fn writer<W: Write>(&self, w: W) -> Self::Writer<W> {
     SpanWriter {
+      config: self.clone(),
       lines: Rc::new(RefCell::new(Lines::default().writer(w))),
       depth: 0,
     }
   }
 }
 
+impl Spans {
+  /// Displays a bracket with a given name and width.\
+  /// This is intended to be used for span annotation.
+  ///
+  /// ## Examples
+  ///
+  /// ```
+  /// # use diom_fmt::formats::spans::Spans;
+  /// # fn test() {
+  /// let span = Spans::default();
+  /// assert_eq!(span.bracket_str("var", 0), "");
+  /// assert_eq!(span.bracket_str("var", 1), "^");
+  ///
+  /// assert_eq!(span.bracket_str("var", 2), "()");
+  /// assert_eq!(span.bracket_str("var", 4), "(  )");
+  /// assert_eq!(span.bracket_str("var", 5), "(var)");
+  /// assert_eq!(span.bracket_str("var", 7), "( var )");
+  ///
+  /// assert_eq!(span.bracket_str("float", 0), "");
+  /// assert_eq!(span.bracket_str("float", 1), "^");
+  /// assert_eq!(span.bracket_str("float", 5), "(   )");
+  /// assert_eq!(span.bracket_str("float", 7), "(float)");
+  /// assert_eq!(span.bracket_str("float", 8), "(float )");
+  /// # }
+  /// ```
+  pub fn bracket_str(&self, name: &str, width: usize) -> String {
+    let Spans {
+      single,
+      open,
+      close,
+      fill,
+    } = self;
+    if width == 0 {
+      return "".into();
+    }
+    if width == 1 {
+      return single.to_string();
+    }
+
+    let t_len = width - 2; // total
+    if t_len < name.len() {
+      return format!(
+        "{open}{tfill}{close}",
+        tfill = fill.to_string().repeat(t_len),
+      );
+    }
+
+    let r_len = t_len - name.len(); //  remaining
+    let f_len = r_len / 2; //           front
+    let b_len = f_len + (r_len % 2); // back
+
+    format!(
+      "{open}{ffill}{name}{bfill}{close}",
+      ffill = fill.to_string().repeat(f_len),
+      bfill = fill.to_string().repeat(b_len),
+    )
+  }
+}
 pub struct SpanWriter<W> {
+  /// The config for how to display spans
+  config: Spans,
   /// The lines that the should be displayed
   ///
   /// ## Todo
@@ -62,6 +152,7 @@ impl<W: Write> Flush for SpanWriter<W> {
 impl<W: Write> SpanWriter<W> {
   pub fn child(&mut self) -> SpanWriter<W> {
     SpanWriter {
+      config: self.config.clone(),
       lines: self.lines.clone(),
       depth: self.depth + 1,
     }
@@ -69,23 +160,10 @@ impl<W: Write> SpanWriter<W> {
 
   pub fn bracket(&mut self, name: impl AsRef<str>, range: &Range<usize>) -> std::fmt::Result {
     let Range { start, end } = *range;
-    self
-      .lines
-      .borrow_mut()
-      .write_at([start, self.depth], bracket_str(name.as_ref(), end - start));
+    self.lines.borrow_mut().write_at(
+      [start, self.depth],
+      self.config.bracket_str(name.as_ref(), end - start),
+    );
     Ok(())
   }
-}
-
-pub fn bracket_str(name: &str, width: usize) -> String {
-  if width == 0 {
-    return "".to_string();
-  }
-  if width == 1 {
-    return '^'.to_string();
-  }
-  if width < name.len() + 2 {
-    return String::from('(') + &" ".repeat(width - 2) + ")";
-  }
-  format!("({: ^1$})", name, width - 2)
 }
