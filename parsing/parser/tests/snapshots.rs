d4a2a2ca0c294a645dbde9803a573dfecf5fc87d
Alex Colby <alexdcolby@pm.me>
2026-03-07 13:07:56 +0000

    test: adds tests for most float literals

--- parsing/parser/tests/snapshots.rs
+++ parsing/parser/tests/snapshots.rs
@@ -24,6 +24,29 @@ fn quick_parse(tokens: SpanTokens) -> Expression<SpanTokens> {
 }
 
 #[test]
+fn floats() {
+  let tokens = quick_lex("5");
+  let expr = quick_parse((&tokens).into());
+  assert_debug_snapshot!(expr);
+
+  let tokens = quick_lex("5.0");
+  let expr = quick_parse((&tokens).into());
+  assert_debug_snapshot!(expr);
+
+  let tokens = quick_lex("5e0");
+  let expr = quick_parse((&tokens).into());
+  assert_debug_snapshot!(expr);
+
+  let tokens = quick_lex("5e-1");
+  let expr = quick_parse((&tokens).into());
+  assert_debug_snapshot!(expr);
+
+  let tokens = quick_lex("- 0.5e-2");
+  let expr = quick_parse((&tokens).into());
+  assert_debug_snapshot!(expr);
+}
+
+#[test]
 fn functions() {
   let tokens = quick_lex("() => 5");
   let expr = quick_parse((&tokens).into());
