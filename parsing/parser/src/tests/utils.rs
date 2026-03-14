4fa7b2579d4904d4239606ddb46ec8009f554532
Alex Colby <alexdcolby@pm.me>
2026-03-14 17:54:30 +0000

    test: improves error messages by giving a full stack trace

--- parsing/parser/src/tests/utils.rs
+++ parsing/parser/src/tests/utils.rs
@@ -2,7 +2,8 @@ use crate::{parse_node, SyntaxNode};
 use diom_lexer::parse_tokens;
 use diom_syntax::expressions::Expression;
 use diom_tokens::{SpanToken, SpanTokens};
-use nom::{combinator::all_consuming, error::Error, Err, Offset, Parser};
+use nom::{combinator::all_consuming, Err, Offset, Parser};
+use nom_language::error::VerboseError;
 use std::fmt::{from_fn, Debug};
 
 pub fn quick_lex(code: &str) -> Vec<SpanToken<'_>> {
@@ -18,9 +19,12 @@ pub fn quick_parse<'a>(code: &'a str, tokens: SpanTokens<'a>) -> Expression<Span
   let (_, node) = all_consuming(parse_node)
     .parse(tokens)
     .map_err(|err| {
-      err.map(|err: Error<_>| Error {
-        input: err.input.0[0].origin,
-        code: err.code,
+      err.map(|err: VerboseError<_>| VerboseError {
+        errors: err
+          .errors
+          .into_iter()
+          .map(|(input, kind)| (input.0[0].origin, kind))
+          .collect(),
       })
     })
     .map_err(|err| format_error(code, err))
@@ -29,23 +33,27 @@ pub fn quick_parse<'a>(code: &'a str, tokens: SpanTokens<'a>) -> Expression<Span
   expr
 }
 
-fn format_error<'a>(input_: &'a str, error: Err<Error<&'a str>>) -> impl Debug + 'a {
+fn format_error<'a>(input_: &'a str, error: Err<VerboseError<&'a str>>) -> impl Debug + 'a {
   from_fn(move |f| {
-    writeln!(f, "\n`{input_}`")?;
+    write!(f, "\n`{input_}`")?;
     if let Err::Incomplete(n) = error {
-      f.write_str(&" ".repeat(input_.len() + 1))?;
+      write!(f, "\n{}", " ".repeat(input_.len() + 1))?;
       write!(f, "^ Needed ({n:?})")?;
       return Ok(());
     }
 
-    let (kind, Error { input, code }) = match &error {
+    let (kind, VerboseError { errors }) = match &error {
       Err::Error(e) => ("error", e),
       Err::Failure(e) => ("failure", e),
       _ => unreachable!("we've handled incomplete"),
     };
+    write!(f, " had {}", kind)?;
 
-    let i = input_.offset(input);
-    f.write_str(&" ".repeat(i + 1))?;
-    write!(f, "^ {code:?} ({kind})")
+    for (input, kind) in errors {
+      let i = input_.offset(input);
+      write!(f, "\n{}", " ".repeat(i + 1))?;
+      write!(f, "^ {kind:?}")?;
+    }
+    Ok(())
   })
 }
